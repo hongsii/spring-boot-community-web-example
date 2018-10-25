241a37fc58498c64391f23747bf5f84a81a8d0a8
Sihong Park <pozt1234@gmail.com>
2018-10-25 22:53:49 +0900

    Initial commit

---
+++ src/main/java/com/hongsii/springboot/community/web/example/SpringBootCommunityWebExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hongsii.springboot.community.web.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityWebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommunityWebExampleApplication.class, args);
+	}
+}
